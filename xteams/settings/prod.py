a3532083de8d6161749ff299048eecece18ec749
Tom Gurion <nagasaki45@gmail.com>
2015-12-25 20:52:53 +0200

    Set DEBUG = False in production

--- xteams/settings/prod.py
+++ xteams/settings/prod.py
@@ -4,6 +4,8 @@ from .base import *
 KEY_CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
 SECRET_KEY = ''.join([SystemRandom().choice(KEY_CHARS) for i in range(50)])
 
+DEBUG = False
+
 # The db docker container
 DATABASES['default']['HOST'] = 'db'
 
