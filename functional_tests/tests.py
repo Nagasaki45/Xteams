0843a874d8d348f7339ebdf4715474544f7b95c8
Tom Gurion <nagasaki45@gmail.com>
2016-02-14 22:11:57 +0200

    Add some basic functional tests (selenium). Fix #11

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,68 +1,109 @@
-from django.test import LiveServerTestCase
+import unittest
 
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
-from teams.models import Team, Player
+from groups.models import Group, Player
 
 
-class SiteVisitorTest(LiveServerTestCase):
+class TeamsTest(StaticLiveServerTestCase):
 
     def setUp(self):
-        nv = Team.objects.create(name='Nahlaot Volleyball')
-        ts = Team.objects.create(name='Tel-Aviv Socker')
+        nv = Group.objects.create(name='Nahlaot Volleyball')
+        ts = Group.objects.create(name='Tel-Aviv Socker')
 
-        Player.objects.create(name='Noam', score=4, team=nv)
-        Player.objects.create(name='Ron', score=9, team=nv)
-        Player.objects.create(name='Guy', score=3, team=nv)
-        Player.objects.create(name='Yael', score=5, team=nv)
-        Player.objects.create(name='Yaniv', score=2, team=ts)
-        Player.objects.create(name='Arik', score=8, team=ts)
-        Player.objects.create(name='Gonen', score=6, team=ts)
-        Player.objects.create(name='Jacob', score=9, team=ts)
+        Player.objects.create(name='Noam', score=4, group=nv)
+        Player.objects.create(name='Ron', score=9, group=nv)
+        Player.objects.create(name='Guy', score=3, group=nv)
+        Player.objects.create(name='Yael', score=5, group=nv)
+        Player.objects.create(name='Yaniv', score=2, group=ts)
+        Player.objects.create(name='Arik', score=8, group=ts)
+        Player.objects.create(name='Gonen', score=6, group=ts)
+        Player.objects.create(name='Jacob', score=11, group=ts)
 
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(5)  # set session timeout for queries
+        # self.browser.implicitly_wait(5)  # set session timeout for queries
 
     def tearDown(self):
         self.browser.close()
 
-    def click_player_checkbox(self, player):
-        checkbox = self.browser.find_element_by_id('id_' + player)
-        checkbox.click()
+    def change_player_state(self, player, button_text):
+        xpath_template = "//a[contains(., '{}')]/span[contains(., '{}')]/p"
+        xpath = xpath_template.format(player, button_text)
+        self.browser.find_element_by_xpath(xpath).click()
 
-    def test_user_story(self):
+    def parse_teams(self):
+        """When on the 'teams' page, returns a set of sets of players."""
+        # Sanity check, to make sure I'm on the right page
+        header = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('suggested teams', header.text)
+
+        teams = set()
+        dom_teams = self.browser.find_elements_by_css_selector('.list-group')
+        for dom_team in dom_teams:
+            players = dom_team.find_elements_by_tag_name('a')
+            players.pop(0)  # Remove the team name from the list
+            teams.add(frozenset(player.text for player in players))
+        return teams
+
+    def click_create_teams(self):
+        xpath = "//button[contains(., 'Create teams!')]"
+        self.browser.find_element_by_xpath(xpath).click()
+
+    def set_num_of_teams(self, num_of_teams):
+        inputbox = self.browser.find_element_by_id('id_number_of_teams')
+        inputbox.clear()
+        inputbox.send_keys(num_of_teams)
+
+    def test_simple_two_teams_creation(self):
         # Moshe enter the site
         self.browser.get(self.live_server_url)
 
         # He sees the right title
         self.assertIn('Xteams!', self.browser.title)
 
-        # He looks at the list of available teams
-        teams = self.browser.find_element_by_id('teams')
+        # He clicks the Nahlaot Volleyball group
+        self.browser.find_element_by_link_text('Nahlaot Volleyball').click()
 
-        # He clicks the Nahlaot Volleyball team
-        team = self.browser.find_element_by_link_text('Nahlaot Volleyball')
-        team.click()
+        # He move some players to the court and creates teams
+        self.change_player_state('Noam', 'Move to court')
+        self.change_player_state('Ron', 'Move to court')
+        self.click_create_teams()
 
-        # He looks at the list of available players
-        players = self.browser.find_element_by_id('players')
+        # Now he sees the results
+        teams = self.parse_teams()
+        expected = {frozenset({'Ron'}), frozenset({'Noam'})}
+        self.assertEqual(teams, expected)
 
-        # He select the inputbox to enter how many groups he wants
-        inputbox = self.browser.find_element_by_id('id_num_of_groups')
+    def test_not_enough_players_to_create_teams(self):
+        # Now, Moshe wants to create 3 teams
+        # but there are only 2 players on the court
+        group = Group.objects.get(name='Nahlaot Volleyball')
+        self.browser.get(self.live_server_url + group.get_absolute_url())
 
-        # And enter 3 (as for 3 groups)
-        inputbox.send_keys('3')
+        self.change_player_state('Noam', 'Move to court')
+        self.change_player_state('Ron', 'Move to court')
 
-        # He checks some players:
-        self.click_player_checkbox('Noam')
-        self.click_player_checkbox('Ron')
-        self.click_player_checkbox('Guy')
-        self.click_player_checkbox('Yael')
+        self.set_num_of_teams(3)
+        self.click_create_teams()
 
-        # And click on submit.
-        submit = self.browser.find_element_by_tag_name('button')
-        submit.click()
+        error = self.browser.find_element_by_css_selector('.alert-danger')
+        self.assertIn('not enough players', error.text.lower())
 
-        # Moshe now see the results
-        self.fail('finish functional test')
+    def test_sane_team_up(self):
+        # Moshe wants to create teams for real, and expect the teams to have
+        # similar overal strength
+        group = Group.objects.get(name='Tel-Aviv Socker')
+        self.browser.get(self.live_server_url + group.get_absolute_url())
+
+        self.change_player_state('Yaniv', 'Move to court')  # score: 2
+        self.change_player_state('Arik', 'Move to court')  # score: 8
+        self.change_player_state('Gonen', 'Move to court')  # score: 6
+        self.change_player_state('Jacob', 'Move to court')  # score: 11
+
+        self.click_create_teams()
+
+        teams = self.parse_teams()
+        expected = {frozenset({'Yaniv', 'Jacob'}),
+                    frozenset({'Arik', 'Gonen'})}
+        self.assertEqual(teams, expected)
