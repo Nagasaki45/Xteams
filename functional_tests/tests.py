af00e1371f2c4a795b0493d40aa6169e37b56d7d
Tom Gurion <nagasaki45@gmail.com>
2016-02-18 20:08:23 +0200

    Add register / login / logout functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 import unittest
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.contrib.auth import get_user_model
 from selenium import webdriver
+
+User = get_user_model()
 
 from groups.models import Group, Player
 
@@ -106,3 +109,61 @@ class TeamsTest(StaticLiveServerTestCase):
         expected = {frozenset({'Yaniv', 'Jacob'}),
                     frozenset({'Arik', 'Gonen'})}
         self.assertEqual(teams, expected)
+
+
+class AccountManagementTest(StaticLiveServerTestCase):
+
+    def setUp(self):
+        User.objects.create_user(username='Yossi', password='secret_password')
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+
+    def tearDown(self):
+        self.browser.close()
+
+    def fill_input_field(self, placeholder, content):
+        xpath = "//input[@placeholder='{}']".format(placeholder)
+        input_field = self.browser.find_element_by_xpath(xpath)
+        input_field.send_keys(content)
+
+    def submit(self):
+        self.browser.find_element_by_xpath("//input[@type='submit']").click()
+
+    def assertUserLoggedOut(self):
+        body_text = self.browser.find_element_by_tag_name('body').text
+        self.assertIn('Login', body_text)
+        self.assertNotIn('Logout', body_text)
+
+    def assertUserLoggedIn(self):
+        body_text = self.browser.find_element_by_tag_name('body').text
+        self.assertIn('Logout', body_text)
+        self.assertNotIn('Login', body_text)
+
+    def test_register(self):
+        self.assertUserLoggedOut()
+
+        self.browser.find_element_by_link_text('Register').click()
+        self.fill_input_field('Username', 'Moshe')
+        self.fill_input_field('Password', 'secret_password')
+        self.fill_input_field('Password confirmation', 'secret_password')
+        self.submit()
+
+        # Once registered users are redirected to home page again
+        self.assertEqual(self.browser.current_url.strip('/'),
+                         self.live_server_url.strip('/'))
+        self.assertUserLoggedIn()
+
+    def test_login_and_logout(self):
+        self.assertUserLoggedOut()
+
+        self.browser.find_element_by_link_text('Login').click()
+        self.fill_input_field('Username', 'Yossi')
+        self.fill_input_field('Password', 'secret_password')
+        self.submit()
+
+        self.assertEqual(self.browser.current_url.strip('/'),
+                         self.live_server_url.strip('/'))
+        self.assertUserLoggedIn()
+
+        self.browser.find_element_by_link_text('Logout').click()
+        self.assertUserLoggedOut()
